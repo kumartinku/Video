594f1261f086f6bd44c8c5b9dd3604e498c2d69b
kumartinku <dilipkmr911@gmail.com>
2017-04-12 15:18:17 +0530

    this is simple

---
+++ app/src/main/java/com/example/dilippc/video/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.dilippc.video;
+
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+     b=(Button) findViewById(R.id.button);
+
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+
+        //Displays a video file.
+        VideoView mVideoView = (VideoView)findViewById(R.id.videoView);
+
+
+       // String uripath="android.resource://com.example.dilippc.video/"+R.raw.spinner;
+       // Uri uri = Uri.parse(uripath);
+       // mVideoView.setVideoURI(uri);
+       // mVideoView.requestFocus();
+      //  mVideoView.start();
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                VideoView videoView=(VideoView) findViewById(R.id.videoView);
+
+                String uripath="android.resource://com.example.dilippc.video/"+R.raw.spinner;
+                Uri uri=Uri.parse(uripath);
+                videoView.setVideoURI(uri);
+                videoView.requestFocus();
+                videoView.start();
+
+            }
+        });
+
+    }
+
+
+}
